9bae4578a00fbefa6bc99ef7cffecd80be70adbc
JhoverAMulluniJove <jhovermullunijove@gmai.com>
2023-12-06 16:08:09 -0500

    Arreglando pagina

---
+++ MODELO/index.php
@@ -0,0 +1,16 @@
+<?php
+// Ruta del archivo HTML
+$rutaArchivoHTML = 'C:\xampp\htdocs\PROYECTO-ING-WEB\VISTAAUX/index.html';
+
+// Intentar abrir y leer el archivo HTML
+$contenidoHTML = file_get_contents($rutaArchivoHTML);
+
+// Verificar si la lectura fue exitosa
+if ($contenidoHTML !== false) {
+    // El archivo se leyó correctamente, puedes imprimir su contenido o realizar otras operaciones
+    echo $contenidoHTML;
+} else {
+    // Ocurrió un error al intentar leer el archivo
+    echo 'No se pudo leer el archivo HTML.';
+}
+?>
--- MODELO/login.php
+++ MODELO/login.php
@@ -1,4 +1,44 @@
 <?php
 session_start();
+$host_db = "localhost";
+$user_db = "root";
+$pass_db = "artescocielo10";
+$db_name = "ferreteria";
+$tbl1_name = "usuario";
+$conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
 
+// Verificar la conexión
+if ($conexion->connect_error) {
+    die("La conexión falló: " . $conexion->connect_error);
+}
+
+$nombre = $_POST['nombre_usuario'];
+$clave = $_POST['password_usuario'];
+
+$sql = "SELECT * FROM $tbl1_name WHERE nombre_usuario = '$nombre'";
+$result = $conexion->query($sql);
+
+// Verificar la ejecución de la consulta
+if (!$result) {
+    die("Error en la consulta: " . $conexion->error);
+}
+
+// Verificar si hay filas devueltas
+if ($result->num_rows > 0) {
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+
+    if ($clave == $row['password_usuario']) {
+        $_SESSION['loggedin'] = true;
+        $_SESSION['nombre_usuario'] = $nombre;
+        $_SESSION['start'] = time();
+        $_SESSION['expire'] = $_SESSION['start'] + (5 * 60); 
+        include("index.php");
+    } else {
+        echo "Nombre o Contraseña están incorrectos.";
+    }
+} else {
+    echo "Usuario no encontrado.";
+}
+
+mysqli_close($conexion);
 ?>
